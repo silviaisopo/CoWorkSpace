f9f47820b8c9ee660ace486b8d682ad7a4c0763a
silviaisopo <silvia.isopo@gmail.com>
2025-08-31 22:13:25 +0200

    aggiornamento responsività

--- public/js/app.js
+++ public/js/app.js
@@ -189,4 +189,10 @@ function removePendingBooking() {
     updateCartBadge();
 }
 
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+hamburgerBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
 
